372941c7a1f0fab30f55d4e19a5a5d0b0faf4815
gDiode <a.g.ben.min@gmail.com>
2023-06-29 14:16:37 -0600

    Update agent_port0.sv

--- verif/env/agent_port0.sv
+++ verif/env/agent_port0.sv
@@ -1,3 +1,9 @@
 class agent_port0 extends uvm_agent
 	`uvm_component_utils(agent_port0)
+	
+	
+	function new(input string name, uvm_component parent);
+		super.new(name,parent);
+	endfunction
+	
 endclass
