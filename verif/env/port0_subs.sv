3aca92785b063123ececf374820028d8f4f22d2d
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-02 22:42:14 -0600

    Update port0_subs.sv

--- verif/env/port0_subs.sv
+++ verif/env/port0_subs.sv
@@ -1,7 +1,18 @@
 /*
 Atribute: port0_subs
 Author: Ana Paula Godoy Monroy
-Date: 
-Version:
+Date: 02/07/2023
+Version: 01
 File name: port0_subs.sv
 */
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+virtual class port0_subs extends uvm_subscriber;
+  `uvm_component_utils(port0_subs)
+
+  function new (string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction 
+
+endclass : port0_scb
