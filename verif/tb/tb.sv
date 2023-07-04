b7f6956f1f7bd8d88fb6fa7e8914ab0670152917
Xrist12 <133039993+Xrist12@users.noreply.github.com>
2023-07-04 13:16:38 -0600

    Update tb.sv
    Update by Cristian

--- verif/tb/tb.sv
+++ verif/tb/tb.sv
@@ -6,11 +6,19 @@ Version:
 File name: 
 */
 
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
 module tb;
+  //clock and reset variables//
+	bit clk;
+	bit rstn;
+  
+  // interfaces instances //
   port0_intf intf0(clk0);
   port1_intf intf1(clk1);
   
-ram_proj DUT
+  ram_proj DUT
   	 (.clk0(intf0.clk0),
       .cs0(intf0.cs0),		
       .we0(intf0.we0),
@@ -23,7 +31,23 @@ ram_proj DUT
       .addr1(intf1.addr1),
       .dout1(intf1.dout1));
   
+  // generate clk and rstn signals //
+  always clk = #5 ~clk;
 
+  initial begin
+		rstn = '0;
+		#50;
+		rstn = 'b1;
+	end
+
+  // Connecting TLM ports and sending interfaces to transactors (monitors and drivers) //
+	initial begin
+    uvm_config_db#(virtual port0_intf)::set(null,"*","port0_intf",intf0);
+    uvm_config_db#(virtual port1_intf)::set(null,"*","port1_intf",intf1);
+
+    
+	end
+  
   initial begin 
     run_test();
   end
