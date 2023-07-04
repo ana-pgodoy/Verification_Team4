3d3707476b8de769c5df857cdc23e246df14af7d
gDiode <a.g.ben.min@gmail.com>
2023-07-03 21:13:26 -0600

    Anadido los modulos actuales al pkg

---
+++ verif/env/port0_agent/agent_port0_pkg.sv
@@ -0,0 +1,20 @@
+/*
+
+Disenador: Benjamin Gonzalez Alvarado
+Modulo: agent_port0_pkg
+Compania: Cinvestav
+Contacto: a.g.ben.min@gmail.com
+
+*/
+package agent_port0_pkg;
+	import uvm_pkg::*;
+	`include "uvm_macros.svh"
+	
+	`include "port0_monitor.svh"
+	`include "port0_driver.svh"
+	`include "port0_scb.svh"
+	`include "port0_sqr.svh"
+	`include "port0_subs.svh"
+	`include "agent_port0.svh"
+	
+endpackage
