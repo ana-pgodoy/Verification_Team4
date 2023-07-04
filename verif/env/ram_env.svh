4c0c9d94280b3569e797b5b68054ad63640a05d7
ana-pgodoy <anna.pgm28@gmail.com>
2023-07-03 18:20:12 -0600

    [ENV] ram_env file was created

---
+++ verif/env/ram_env.svh
@@ -0,0 +1,25 @@
+/*
+Atribute: enviroment
+Author: 
+Date: 03/07/2023
+Version: 01
+File name: ram_env.svh
+*/
+class ram_env extends uvm_env;
+	`uvm_component_utils(ram_env)
+	
+	port0_agent agent_0;
+	port1_agent agent_1;
+	
+  function new (string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction 
+  
+   virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    `uvm_info(get_name(), "Build Phase", UVM_MEDIUM)
+	agent_0=port0_agent::type_id::create("agent_0",this);
+	agent_1=port1_agent::type_id::create("agent_1",this);
+  endfunction
+  
+endclass : ram_env
