410a478955f26f08064b1530a95b8a7e3772e10a
Xrist12 <133039993+Xrist12@users.noreply.github.com>
2023-07-04 12:54:21 -0600

    Update port1_scb.sv

--- verif/env/port1_agent/port1_scb.sv
+++ verif/env/port1_agent/port1_scb.sv
@@ -6,45 +6,28 @@ Version:
 File name: port1_scb
 */
 
-class mem_scoreboard extends uvm_scoreboard;
+class port1_scb extends uvm_scoreboard;
+	`uvm_component_utils(port1_scb)
 
-   `uvm_component_utils(mem_scoreboard)
-   
+	uvm_analysis_export #(uvm_sequence_item) ana_export;
+	uvm_tlm_analysis_fifo#(uvm_sequence_item) ana_fifo;
+
+	function new(string name, uvm_component parent);
+		super.new(name,parent);
+		ana_export = new("ana_export",this);
+		ana_fifo = new("ana_fifo",this);
+	endfunction
+
+	function void connect_phase(uvm_phase phase);
+		super.connect_phase(phase);
+		ana_export.connect(ana_fifo.analysis_export);
+	endfunction
+
    bit [31:0] sc_mem [9:0];
-
-   // new - constructor
-   function new (string name, uvm_component parent);
-      super.new(name, parent);
-   endfunction : new
-
-   //---------------------------------------
-  // build_phase - create port and initialize local memory
-  //---------------------------------------
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-      item_collected_export = new("item_collected_export", this);
-      foreach(sc_mem[i]) sc_mem[i] = 8'hFF;
-  endfunction: build_phase
-
-   //Declaring port
-   uvm_analysis_imp#(mem_seq_item, mem_scoreboard) item_collected_export;
-
-   //creating port
-   item_collected_export = new("item_collected_export", this);
-
-   monitor.item_collected_port.connect(scoreboard.item_collected_export);
-   
-   //calling write method from monitor
-   item_collected_port.write(pkt);
-   
-   //scoreboard write function
-   virtual function void write(mem_seq_item pkt);
-      pkt.print();
-   endfunction : write
 
    // run phase
    virtual task run_phase(uvm_phase phase);
-      mem_seq_item mem_pkt;
+      uvm_sequence_item mem_pkt;
       //--- comparision logic ---  //
       forever begin
          wait(pkt.size() > 0);
@@ -66,6 +49,4 @@ class mem_scoreboard extends uvm_scoreboard;
       end
    endtask : run_phase
 
-
-
-endclass : mem_scoreboard
+endclass
