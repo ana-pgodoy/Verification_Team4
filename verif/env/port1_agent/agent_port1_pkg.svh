59fe39a21c2fdd2b3545459c7b5429f0070e0393
Pau-Nunez <133161674+Pau-Nunez@users.noreply.github.com>
2023-07-04 15:02:25 -0600

    agent_port1_pkg.svh

---
+++ verif/env/port1_agent/agent_port1_pkg.svh
@@ -0,0 +1,20 @@
+/*
+
+Disenador: Paulina Vianney Núñez Luna
+Modulo: agent_port1_pkg
+Compania: Cinvestav
+Contacto: paulinav.nunezl@gmail.com
+
+*/
+package agent_port1_pkg;
+	import uvm_pkg::*;
+	`include "uvm_macros.svh"
+	
+	`include "port1_monitor.svh"
+	`include "port1_driver.svh"
+	`include "port1_scb.svh"
+	`include "port1_sqr.svh"
+	`include "port1_subs.svh"
+	`include "agent_port1.svh"
+	
+endpackage
