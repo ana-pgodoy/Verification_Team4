b3330b0d673e040cd45f7cff61578df02256975b
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-04 19:24:09 -0600

    [intf] port0_intf.sv updated

--- verif/intf/port0_intf.sv
+++ verif/intf/port0_intf.sv
@@ -1,18 +1,19 @@
 /*
 Atribute: port0_intf
 Author: Ana Paula Godoy Monroy
-Date: 
-Version:
+Date: 04/07/2023
+Version:01
 File name: port0_intf.sv
 */
 interface port0_intf(input logic clk0);
-  
-	logic               	 cs0,		
-	logic               	 we0,
-	logic [3:0]				 wmask0,
-	logic [10:0] addr0,
-	logic [32:0] din0,
-	logic [32:0] dout0,
+  	//Inputs
+	bit            cs0;		
+	bit            we0;
+	bit   	[3:0]  wmask0;
+	bit	[10:0] addr0;
+	bit	[32:0] din0;
+	//Output
+	logic [32:0] dout0;
   
 
 endinterface
