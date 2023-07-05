79fb70395ef00dc19b694214e6bb588eef07e25f
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-05 10:49:49 -0600

    [agent_port0] port0_subs updated

--- verif/env/port0_agent/port0_subs.svh
+++ verif/env/port0_agent/port0_subs.svh
@@ -5,14 +5,14 @@ Date: 02/07/2023
 Version: 01
 File name: port0_subs.sv
 */
-class port0_subs extends uvm_subscriber #(uvm_sequence_item) ;
+class port0_subs extends uvm_subscriber #(port0_transaction) ;
   `uvm_component_utils(port0_subs)
 
   function new (string name, uvm_component parent);
     super.new(name, parent);
   endfunction 
   
-  function void write(uvm_sequence_item t);
+  function void write(port0_transaction t);
 	  `uvm_info(get_name(), "Subscriber", UVM_MEDIUM)
   endfunction 
 
