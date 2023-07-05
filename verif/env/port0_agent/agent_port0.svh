497fbd56df98599216ba25b02af4037250d75dd2
gDiode <a.g.ben.min@gmail.com>
2023-07-05 04:47:27 -0600

    Update agent_port0.svh

--- verif/env/port0_agent/agent_port0.svh
+++ verif/env/port0_agent/agent_port0.svh
@@ -20,23 +20,28 @@ class agent_port0 extends uvm_agent
 	//sequencer
 	//MISDATOS nombre filler mientras tanto
 	port0_sqr #(MISDATOS)sequencer;
-	
+	//FALTAN LOS INOUTS
 	function new(input string name, uvm_component parent);
 		super.new(name,parent);
 	endfunction
 	
 	function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
-		//monitor=port0_monitor::type_id::create("monitor",this);
-		//scoreboard
-		//driver
-		//monitor
-		//sequencer
+		
+		monitor=port0_monitor::type_id::create("monitor",this);
+		scoreboard=port0_scb::type_id::create("scoreboard",this);
+		driver=port0_driver::type_id::create("driver",this);
+		monitor=port0_monitor::type_id::create("monitor",this);
+		sequencer=port0_sqr::type_id::create("sequencer",this);
+		//TAMBIEN AQUI FALTAN LOS INOUTS
+		
 	endfunction
 	
 	virtual function void connect_phase(uvm_phase phase);
 		super.connect_phase(phase);
 		`uvm_info(get_name(), "Connect Phase", UVM_NONE)
+		
+		
 	endfunction
   
 	virtual task run_phase(uvm_phase phase);
