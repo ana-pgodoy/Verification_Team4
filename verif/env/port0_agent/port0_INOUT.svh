552f95ce95d263d0b198342d1fb9402e64302f86
gDiode <a.g.ben.min@gmail.com>
2023-07-05 10:33:21 -0600

    function new added

--- verif/env/port0_agent/port0_INOUT.svh
+++ verif/env/port0_agent/port0_INOUT.svh
@@ -10,13 +10,18 @@ Contacto: a.g.ben.min@gmail.com
 
 class port0_INOUT extends uvm_sequence_item;
 
-	logic            			cs0;		
-	logic            			we0;
-	logic   [WMASK_WIDTH-1:0]  	wmask0;
-	logic	[ADDRESS_WIDTH-1:0] addr0;
-	logic	[DATA_WIDTH-1:0] 	din0;
+	bit         	   			cs0;		
+	bit      	      			we0;
+	bit 	[WMASK_WIDTH-1:0]  	wmask0;
+	bit		[ADDRESS_WIDTH-1:0] addr0;
+	bit		[DATA_WIDTH-1:0] 	din0;
 	//Output
 	logic 	[DATA_WIDTH-1:0] 	dout0;
+	
+	
+	function new(string name = "port0_INOUT");
+		super.new(name);
+	endfunction
 	
 	
 	
