31ab5236e94e12471b1411cb438a9e107a472493
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-05 10:53:59 -0600

    [intf] port0_intf updated

--- verif/intf/port0_intf.sv
+++ verif/intf/port0_intf.sv
@@ -7,13 +7,13 @@ File name: port0_intf.sv
 */
 interface port0_intf(input logic clk0);
   	//Inputs
-	bit            cs0;		
-	bit            we0;
-	bit   	[3:0]  wmask0;
-	bit	[10:0] addr0;
-	bit	[32:0] din0;
+	bit          		    cs0;		
+	bit         		    we0;
+	bit   	[WMASK_WIDTH-1:0]   wmask0;
+	bit	[ADDRESS_WIDTH-1:0] addr0;
+	bit	[DATA_WIDTH-1:0]    din0;
 	//Output
-	logic [32:0] dout0;
+	logic   [DATA_WIDTH-1:0]    dout0;
   
 
 endinterface
