85ebd8bb9f3613242e4caefe16b308bc9ef088d7
gDiode <a.g.ben.min@gmail.com>
2023-07-05 10:22:26 -0600

    Clase base e identificador

---
+++ verif/env/port0_agent/port0_INOUT.svh
@@ -0,0 +1,13 @@
+/*
+
+Disenador: Benjamin Gonzalez Alvarado
+Modulo: port0_sqr
+Compania: Cinvestav
+Contacto: a.g.ben.min@gmail.com
+
+*/
+
+
+class port0_INOUT extends uvm_sequence_item;
+
+endclass
