fc6f372f21b3abdad4e04ab79b553f59b3d8b7b0
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-05 11:06:08 -0600

    [defines] ram_defines_pkg. created

---
+++ verif/defines/ram_defines_pkg.sv
@@ -0,0 +1,12 @@
+package ram_defines_pkg;
+  import uvm_pkg::*;
+	`include "uvm_macros.svh"
+//Parameters port0
+  `define WMASK_WIDTH       3'h4
+  `define ADDR_WIDTH        4'hA
+  `define DATA_WIDTH        6'h20
+
+//Parameters port1  
+  `define ADDR_WIDTH_1      4'hA
+  `define WMASK_WIDTH_1     6'h20
+endpackage
