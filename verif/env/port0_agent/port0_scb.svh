dd73aee891dad5be4bc257882aaa142728b851ba
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-04 19:18:49 -0600

    [port0_agent] port0_scb.svh corrected

--- verif/env/port0_agent/port0_scb.svh
+++ verif/env/port0_agent/port0_scb.svh
@@ -2,17 +2,14 @@
 Atribute: port0_scb
 Author: Ana Paula Godoy Monroy
 Date: 01/07/2023
+update: 04/07/2023
 Version: 01
 File name: port0_scb.sv
 */
-import uvm_pkg::*;
-`include "uvm_macros.svh"
 
 virtual class port0_scb extends uvm_scoreboard;
   `uvm_component_utils(port0_scb)
 
-  virtual port0_intf vif;
-  
   uvm_analysis_export #(uvm_sequence_item) analysis_export0;
 
   function new (string name, uvm_component parent);
