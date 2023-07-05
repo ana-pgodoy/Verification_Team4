2b5e8a3b05f3615fb34fb593f4f9119ae081191b
gDiode <a.g.ben.min@gmail.com>
2023-07-05 10:28:13 -0600

    Agregado los pines parametrizados

--- verif/env/port0_agent/port0_INOUT.svh
+++ verif/env/port0_agent/port0_INOUT.svh
@@ -1,7 +1,7 @@
 /*
 
 Disenador: Benjamin Gonzalez Alvarado
-Modulo: port0_sqr
+Modulo: port0_INOUT
 Compania: Cinvestav
 Contacto: a.g.ben.min@gmail.com
 
@@ -10,4 +10,14 @@ Contacto: a.g.ben.min@gmail.com
 
 class port0_INOUT extends uvm_sequence_item;
 
+	logic            			cs0;		
+	logic            			we0;
+	logic   [WMASK_WIDTH-1:0]  	wmask0;
+	logic	[ADDRESS_WIDTH-1:0] addr0;
+	logic	[DATA_WIDTH-1:0] 	din0;
+	//Output
+	logic 	[DATA_WIDTH-1:0] 	dout0;
+	
+	
+	
 endclass
