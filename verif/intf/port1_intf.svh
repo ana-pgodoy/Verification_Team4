de08ab075421c5d17f47d4bae13868540f161de7
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-06 08:29:51 -0600

    Update port1_intf.svh

--- verif/intf/port1_intf.svh
+++ verif/intf/port1_intf.svh
@@ -8,10 +8,9 @@ interface port1_intf(input logic clk1);
   	//Inputs
 	bit        cs1;		
 	bit        we1;
-	bit   	[WMASK_WIDTH-1:0]   wmask1;
-	bit	[ADDRESS_WIDTH-1:0] addr1;
+	bit	[ADDR_WIDTH_1-1:0] addr1;
 	//Output
-  logic [32:0] dout1;
+	logic [DATA_WIDTH_1:0] dout1;
   
 
 endinterface
