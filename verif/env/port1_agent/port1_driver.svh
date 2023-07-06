13c43b071478c16c4d55ee877b41062726063b9e
Pau-Nunez <133161674+Pau-Nunez@users.noreply.github.com>
2023-07-06 11:11:21 -0600

    port1_driver.svh

--- verif/env/port1_agent/port1_driver.svh
+++ verif/env/port1_agent/port1_driver.svh
@@ -5,15 +5,23 @@ Date: 4/07/2023
 File name: port1_driver
 */
 
-//MISDATOS nombre filler mientras tanto
-class port1_driver extends uvm_driver#(MISDATOS)
+class port1_driver extends uvm_driver#(port1_transaction)
   `uvm_component_utils(port1_driver)
 
+	virtual port1_intf port1_vintf;
+
 	function new(input string name, uvm_component parent);
-      
 		super.new(name,parent);
-      
 	endfunction
 	
+	
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		`uvm_info(get_name(), "build phase", UVM_NONE)
+	endfunction
+	
+	virtual task run_phase(uvm_phase phase);
+		`uvm_info(get_name(), "Run Phase", UVM_NONE)
+	endtask
 
 endclass
