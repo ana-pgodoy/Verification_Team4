7fe32e927fcf8cd893e77075c58f98c03738656f
Pau-Nunez <133161674+Pau-Nunez@users.noreply.github.com>
2023-07-05 23:05:59 -0600

    port1_intf.svh

--- verif/intf/port1_intf.svh
+++ verif/intf/port1_intf.svh
@@ -8,8 +8,8 @@ interface port1_intf(input logic clk1);
   	//Inputs
 	bit        cs1;		
 	bit        we1;
-  bit	[10:0] addr1;
-  	
+	bit   	[WMASK_WIDTH-1:0]   wmask1;
+	bit	[ADDRESS_WIDTH-1:0] addr1;
 	//Output
   logic [32:0] dout1;
   
