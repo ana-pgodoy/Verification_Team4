8e28e4f4926e2b8f96f597ec68498c9c40509db8
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-06 08:27:16 -0600

    Update port0_monitor.svh

--- verif/env/port0_agent/port0_monitor.svh
+++ verif/env/port0_agent/port0_monitor.svh
@@ -6,7 +6,7 @@ Version: 01
 File name: port0_monitor.sv
 */
 
-virtual class port0_monitor extends uvm_monitor;
+class port0_monitor extends uvm_monitor;
 	`uvm_component_utils(port0_monitor)
 	
 	virtual port0_intf intf0; // SystemVerilog virtual interface
@@ -17,17 +17,16 @@ virtual class port0_monitor extends uvm_monitor;
 	analysis_port0=new("analysis_port0",this);
   endfunction 
 	
-  virtual function void build_phase(uvm_phase phase);
+virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
 	  if(!uvm_config_db #(virtual port0_intf)::get(this,"","port0_intf", intf0))
 		  `uvm_fatal(get_name(),"Failed to get interface")
-    endfunction
+endfunction
 		  
   virtual task run_phase(uvm_phase phase);
-		`uvm_info(get_name(), "Run Phase", UVM_MEDIUM)
       port0_transaction data;
       data = port0_transaction::type_id::create("data");
-      	@(posedge intf0.clk);
+	  @(posedge intf0.clk0);
 	        data.cs0 = intf0.cs0;
 		data.we0 = intf0.we0;
 		data.wmask0= intf0.wmask0;
@@ -35,6 +34,5 @@ virtual class port0_monitor extends uvm_monitor;
 	 	data.din0 = intf0.din0;
 	  	data.dout0 = intf0.dout0;
 		analysis_port0.write(data);
-   endtask
-  
+   endtask  
 endclass : port0_monitor
