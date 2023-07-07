abfd7de2fe9b03b05c88d10b0aed7fc0607d8b2e
PaulinaRico <paumax201330@gmail.com>
2023-07-07 14:02:24 +0000

    [WORK] list.f was added

---
+++ verif/work/list.f
@@ -0,0 +1,20 @@
+# rtl
+
+#verification
+    +incdir+../defines
+    ../defines/ram_defines_pkg.sv
+    ../intf/port0_intf.sv
+    ../intf/port1_intf.sv
+    +incdir+../refmodel
+    +incdir+../env/port0_agent
+    ../env/agent_port0_pkg.sv
+    +incdir+../env/port1_agent
+    ../env/agent_port1_pkg.sv
+    +incdir+../env
+    ../env/ram_env_pkg.sv
+    +incdir+../seq
+    +incdir+../test
+    ../test/ram_test_pkg.sv
+    ../tb/tb.sv
+
+
