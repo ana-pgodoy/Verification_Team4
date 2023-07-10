b189bacc8fa26f119801f8a0abc02c9516340ad9
PaulinaRico <paumax201330@gmail.com>
2023-07-10 14:49:39 +0000

    [ENV] add agent_port1_pkg and agent_port0_pkg to the envitoment pkg

--- verif/env/ram_env_pkg.sv
+++ verif/env/ram_env_pkg.sv
@@ -2,6 +2,8 @@ package ram_env_pkg;
 	import uvm_pkg::*;
 	`include "uvm_macros.svh"
 	
+	`include "agent_port1_pkg.sv"
+	`include "agent_port0_pkg.sv"
 	`include "ram_env.svh"
 	`include "ram_scb.svh"
 	
