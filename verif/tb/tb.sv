1392d6db2af6dde7950b6fb41ce35d572c2dd214
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-10 10:09:26 -0600

    [tb] tb.sv was modified

--- verif/tb/tb.sv
+++ verif/tb/tb.sv
@@ -6,10 +6,9 @@ Version:01
 File name: tb.sv
 */
 
-import uvm_pkg::*;
-`include "uvm_macros.svh"
-
 module tb;
+  
+    ram_base_test test;
   //clock variable//
 	bit clk;
   
@@ -17,16 +16,16 @@ module tb;
   port0_intf intf0(clk0);
   port1_intf intf1(clk1);
   
-  ram_proj DUT
+  sky130_sram_1kbyte_1rw1r_32x256_8 DUT
   	 (.clk0(intf0.clk0),
-      .cs0(intf0.cs0),		
-      .we0(intf0.we0),
+      .csb0(intf0.cs0),		
+      .web0(intf0.we0),
       .wmask0(intf0.wmask0),
       .addr0(intf0.addr0),
       .din0(intf0.din0),
       .dout0(intf0.dout0),
       .clk1(intf1.clk1),
-      .cs1(intf1.cs1),		
+      .csb1(intf1.cs1),		
       .addr1(intf1.addr1),
       .dout1(intf1.dout1));
   
@@ -39,6 +38,7 @@ module tb;
     uvm_config_db#(virtual port0_intf)::set(null,"*","port0_intf",intf0);
     uvm_config_db#(virtual port1_intf)::set(null,"*","port1_intf",intf1);
 
+    test =new("ram_base_test", null);
     run_test();
 	end
  
