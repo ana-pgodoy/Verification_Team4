49ab61c665140ceb3eb984d94e698d2230b13fde
PaulinaRico <paumax201330@gmail.com>
2023-07-10 14:30:46 +0000

    [INTF] defines pkg was import in the interface

--- verif/intf/port0_intf.sv
+++ verif/intf/port0_intf.sv
@@ -5,15 +5,18 @@ Date: 04/07/2023
 Version:01
 File name: port0_intf.sv
 */
+
+import ram_defines_pkg::*;
+
 interface port0_intf(input logic clk0);
   	//Inputs
-	bit          		    cs0;		
-	bit         		    we0;
-	bit   	[WMASK_WIDTH-1:0]   wmask0;
-	bit	[ADDR_WIDTH-1:0] addr0;
-	bit	[DATA_WIDTH-1:0]    din0;
+	bit      	    		    cs0;		
+	bit         			    we0;
+	bit   	[`WMASK_WIDTH-1:0]  wmask0;
+	bit		[`ADDR_WIDTH-1:0] 	addr0;
+	bit		[`DATA_WIDTH-1:0]   din0;
 	//Output
-	logic   [DATA_WIDTH-1:0]    dout0;
+	logic   [`DATA_WIDTH-1:0]   dout0;
   
 
 endinterface
--- verif/intf/port1_intf.sv
+++ verif/intf/port1_intf.sv
@@ -4,13 +4,15 @@ Date: 05/07/2023
 Version:01
 File name: port1_intf.svh
 */
+import ram_defines_pkg::*;
+
 interface port1_intf(input logic clk1);
   	//Inputs
-	bit        cs1;		
-	bit        we1;
-	bit	[ADDR_WIDTH_1-1:0] addr1;
+	bit   						cs1;		
+	bit        					we1;
+	bit	  [`ADDR_WIDTH_1-1:0] 	addr1;
 	//Output
-	logic [DATA_WIDTH_1:0] dout1;
+	logic [`DATA_WIDTH_1:0] 	dout1;
   
 
 endinterface
