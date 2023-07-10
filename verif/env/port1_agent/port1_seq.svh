592e539a5032b1a75c74b08d0c2456867dc32582
Xrist12 <133039993+Xrist12@users.noreply.github.com>
2023-07-10 10:02:28 -0600

    [PORT1] Update port1_seq.svh

--- verif/env/port1_agent/port1_seq.svh
+++ verif/env/port1_agent/port1_seq.svh
@@ -1,3 +1,11 @@
+/*
+Atribute: port1_seq
+Author: Cristian Eduardo Gomez de la Torre
+Date: 2/07/2023
+Version:
+File name: port1_seq
+*/
+
 class port1_sequencer extends uvm_sequencer#(port1_transaction);
 	`uvm_component_utils(port1_sequencer)
 
