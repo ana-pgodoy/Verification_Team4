d6db18f587cb542d72fa7209f4336dd859410560
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-10 09:56:18 -0600

    [port1_agent] port1_subs.svh was updated

--- verif/env/port1_agent/port1_subs.svh
+++ verif/env/port1_agent/port1_subs.svh
@@ -5,7 +5,7 @@ class port1_subs extends uvm_subscriber #(port1_transaction);
     super.new(name, parent);
   endfunction
 
-	function void write(uvm_sequence_item t);
+	function void write(port1_transaction t);
 		`uvm_info(get_name(),"conv_subscriber",UVM_MEDIUM)
 	endfunction
 
