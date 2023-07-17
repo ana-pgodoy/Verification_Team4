363e38a69907bdb32426e2c874bd1c7ba381fdd6
Xrist12 <133039993+Xrist12@users.noreply.github.com>
2023-07-17 12:37:28 -0600

    Update port1_base_seq.svh

--- verif/seq/port1_base_seq.svh
+++ verif/seq/port1_base_seq.svh
@@ -1,1 +1,16 @@
+class port1_base_seq extends uvm_sequence #(port1_transaction);
+  `uvm_object_utils(port1_base_seq)
 
+  function new(string name ="");
+    super.new(name);
+  endfunction
+
+  task body();
+    req=port1_transaction::type_id::create("req");
+    start_item(req);
+    assert(req.randomize());//with {req.a>req.b;});
+    finish_item(req);
+    get_response(rsp);
+  endtask
+
+endclass
