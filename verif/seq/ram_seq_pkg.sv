ea947c23588ac1ec777f59b1c332ef153066f39c
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-18 12:51:04 -0600

    [seq] ram_seq_pkg.sv was created

---
+++ verif/seq/ram_seq_pkg.sv
@@ -0,0 +1,10 @@
+package ram_seq_pkg;
+	import uvm_pkg::*;
+	
+	`include "uvm_macros.svh"
+	`include "port0_seq_cfg_obj.svh"
+  `include "port1_seq_cfg_obj.svh"
+  `include "port0_base_seq.svh"
+  `include "port1_base_seq.svh"
+
+endpackage
