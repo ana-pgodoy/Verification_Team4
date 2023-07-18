30a52acd4a375485dffaaa6946d5b37b6d70e3c4
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-18 12:43:59 -0600

    [seq] port0_seq_cfg_obj.sv was created

---
+++ verif/seq/port0_seq_cfg_obj.sv
@@ -0,0 +1,22 @@
+import ram_defines_pkg::*;
+
+class port0_seq_cfg_obj extends uvm_object;
+	bit      	    		    cs0;		
+	bit         			    we0;
+	bit   [`WMASK_WIDTH-1:0]  wmask0;
+	bit		[`ADDR_WIDTH-1:0] 	addr0;
+	bit		[`DATA_WIDTH-1:0]   din0;
+	
+	`uvm_object_utils_begin(port0_seq_cfg_obj)
+		`uvm_field_int(cs0,UVM_DEFAULT)
+		`uvm_field_int(we0,UVM_DEFAULT)
+		`uvm_field_int(wmask0,UVM_DEFAULT)
+		`uvm_field_int(addr0,UVM_DEFAULT)
+		`uvm_field_int(din0,UVM_DEFAULT)
+	`uvm_object_utils_end
+	
+  function new (string name="");
+    super.new(name);
+  endfunction
+ 
+endclass
