ed4eb69a3aa4507b42eba8c656420850ec0bea66
Xrist12 <133039993+Xrist12@users.noreply.github.com>
2023-07-20 14:03:18 -0600

    [seq] Update port1_fast_read_10_seq.svh

--- verif/seq/port1_fast_read_10_seq.svh
+++ verif/seq/port1_fast_read_10_seq.svh
@@ -20,6 +20,11 @@ class port1_fast_read_10_seq extends port1_base_seq
                                req.addr1 == seq_cfg_obj.addr1}
         start_item(req);
         finish_item(req);
+        #10ns;
+        //assert(std::randomize(delay) with {delay > 0 ; delay < 100; })
+        //repeat(delay) begin
+        //   #10ns;
+        //end
       end
    endtask
 endclass
