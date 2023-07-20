08c7925b5303a819bf045d97a5c29d9821bef8ce
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-20 12:42:06 -0600

    [seq] port0_write_10_seq_we updated

--- verif/seq/port0_write_10_seq_we.svh
+++ verif/seq/port0_write_10_seq_we.svh
@@ -9,9 +9,11 @@ class  port0_write_10_seq_we extends port0_base_seq;
    for (int i = 0; i < 10; i++) begin
     req=port0_transaction::type_id::create("req");
     start_item(req);
-    req.randomize with {we0 == 1'b1;};
+     req.randomize with {we0 == 1'b0;
+                        cs0 ==1'b0;};
+     `uvm_info(get_name(), "Generating sequence item", UVM_MEDIUM);
     finish_item(req);
-    get_response(rsp);
+    //get_response(rsp);
   endtask
 
 endclass
