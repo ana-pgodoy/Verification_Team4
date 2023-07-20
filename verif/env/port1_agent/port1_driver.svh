f251dc9ef536f6d37f2da38152d5a6815eebe5b6
Pau-Nunez <133161674+Pau-Nunez@users.noreply.github.com>
2023-07-20 13:09:35 -0600

    port1_driver.svh

--- verif/env/port1_agent/port1_driver.svh
+++ verif/env/port1_agent/port1_driver.svh
@@ -18,6 +18,9 @@ class port1_driver extends uvm_driver#(port1_transaction);
 	function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
 		`uvm_info(get_name(), "build phase", UVM_NONE)
+
+		if(!uvm_config_db#(virtual port1_intf)::get(this,"","port1_intf",port1_vintf))
+			`uvm_fatal(get_name(),"config db failed")
 	endfunction
 	
 	virtual task run_phase(uvm_phase phase);
