3a1a95f50eb3c7c8d79a44ed97b9596d2ef4db3e
Pau-Nunez <133161674+Pau-Nunez@users.noreply.github.com>
2023-07-21 11:00:17 -0600

    port1_read_10_seq.svh

--- verif/seq/port1_read_10_seq.svh
+++ verif/seq/port1_read_10_seq.svh
@@ -8,8 +8,8 @@ class port1_read_10_seq_cs1 extends port1_base_seq
    task body();
       req = port1_transaction::type_id::create("req");
       start_item(req);
-      req.randomize() with { req.width == env.env_cfg.agent_cfg.cs1_width:
-                            req.heigth == env.env_cfg.agent_cfg.cs1_heigth; };
+      req.randomize() with { req.width == env.env_cfg.agent_cfg.cs1;
+                            req.heigth == env.env_cfg.agent_cfg.cs1; };
                   `uvm_info(get_name(),"Generating sequence item",UVM_HIGH)
                   `uvm_info(get_name(),$sformatf("%s",req.sprint(uvm_default_line_printer)),UVM_DEBUG)
       finish_item(req);
