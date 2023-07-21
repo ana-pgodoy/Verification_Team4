a6422a09ee4e9e88347ed955af49de93ff27ef28
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-21 11:07:07 -0600

    [seq] ram_seq_pkg.sv was updated

--- verif/seq/ram_seq_pkg.sv
+++ verif/seq/ram_seq_pkg.sv
@@ -4,8 +4,17 @@ package ram_seq_pkg;
 	import agent_port1_pkg::*;
 	`include "uvm_macros.svh"
 	`include "port0_seq_cfg_obj.svh"
-  `include "port1_seq_cfg_obj.svh"
-  `include "port0_base_seq.svh"
-  `include "port1_base_seq.svh"
+	  `include "port1_seq_cfg_obj.svh"
+	  `include "port0_base_seq.svh"
+	  `include "port1_base_seq.svh"
+	  `include "port0_read_10_seq_cs0.svh"
+	  `include "port0_read_10_seq_cs0_OFF.svh"
+	  `include "port0_read_10_seq_cs0_slow.svh"
+	  `include "port0_write_10_seq_we.svh"
+	  `include "port1_read_10_seq.svh"
+	  `include "port1_read_10_seq_cs1.svh"
+	  `include "port1_slow_read_10_seq.svh"
+  
+  
 
 endpackage
