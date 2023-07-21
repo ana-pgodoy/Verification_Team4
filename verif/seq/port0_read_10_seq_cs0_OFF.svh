8c4bcb9cc4e907577f4160f3f3ba413e7c0aab84
gDiode <a.g.ben.min@gmail.com>
2023-07-21 10:26:31 -0600

    [port0_read_10_seq_cs0_OFF] finished

--- verif/seq/port0_read_10_seq_cs0_OFF.svh
+++ verif/seq/port0_read_10_seq_cs0_OFF.svh
@@ -1,13 +1,13 @@
 /*
 
 Disenador: Benjamin Gonzalez Alvarado
-Modulo: port0_read_10_seq_cs0
+Modulo: port0_read_10_seq_cs0_OFF
 Compania: Cinvestav
 Contacto: a.g.ben.min@gmail.com
 
 */
-class port0_read_10_seq_cs0 extends port0_base_seq;
-	`uvm_object_utils(port0_read_10_seq_cs0)
+class port0_read_10_seq_cs0_OFF extends port0_base_seq;
+	`uvm_object_utils(port0_read_10_seq_cs0_OFF)
 	
 	function new(string name ="port0_read_10_seq_cs0");
 		super.new(name);
@@ -19,7 +19,7 @@ class port0_read_10_seq_cs0 extends port0_base_seq;
 		
 		for (int i = 0; i < 10; i++) begin
 	
-			assert(req.randomize() with {req.cs0 ==0;
+			assert(req.randomize() with {req.cs0 ==1;
 										req.we0==1; });
 			start_item(req);
 			finish_item(req);
