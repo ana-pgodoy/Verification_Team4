1cde8557b1d9518b6d723d5537a4d659258d1674
Xrist12 <133039993+Xrist12@users.noreply.github.com>
2023-07-24 12:57:49 -0600

    Update port1_seq_cfg_obj.svh

--- verif/seq/port1_seq_cfg_obj.svh
+++ verif/seq/port1_seq_cfg_obj.svh
@@ -1,17 +1,17 @@
 import ram_defines_pkg::*;
 
 class port1_seq_cfg_obj extends uvm_object;
-	bit      	    		    cs1;
-  bit		[`ADDR_WIDTH-1:0] 	addr1;
+   bit 		             cs1;
+   bit [`ADDR_WIDTH-1:0] addr1;
 	
-  `uvm_object_utils_begin(port1_seq_cfg_obj)
-    `uvm_field_int(cs1,UVM_DEFAULT)
-    `uvm_field_int(addr1,UVM_DEFAULT)
-	`uvm_object_utils_end
+   `uvm_object_utils_begin(port1_seq_cfg_obj)
+   `uvm_field_int(cs1,UVM_DEFAULT)
+   `uvm_field_int(addr1,UVM_DEFAULT)
+   `uvm_object_utils_end
 	
-  function new (string name="");
-    super.new(name);
-  endfunction
+   function new (string name="");
+      super.new(name);
+   endfunction
  
 endclass
 
