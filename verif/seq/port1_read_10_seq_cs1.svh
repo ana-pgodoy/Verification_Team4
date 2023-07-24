98bf099693c13023db27294e881fc30ce763f146
Pau-Nunez <133161674+Pau-Nunez@users.noreply.github.com>
2023-07-24 11:03:24 -0600

    port1_read_10_seq_cs1.svh

--- verif/seq/port1_read_10_seq_cs1.svh
+++ verif/seq/port1_read_10_seq_cs1.svh
@@ -1,3 +1,11 @@
+/*
+Atribute: port1_read_10_seq_cs1
+Author: Paulina Vianney Núñez Luna
+Date: 20/07/2023
+Version: 1
+File name: port1_read_10_seq_cs1.svh
+*/
+
 class port1_read_10_seq_cs1 extends port1_base_seq;
 
 endclass
