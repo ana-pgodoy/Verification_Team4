d6ed55631ac8a35c04c95b17b4925bf159b2d039
Pau-Nunez <133161674+Pau-Nunez@users.noreply.github.com>
2023-07-24 12:44:09 -0600

    port1_read_10_seq_cs1.svh

--- verif/seq/port1_read_10_seq_cs1.svh
+++ verif/seq/port1_read_10_seq_cs1.svh
@@ -7,5 +7,24 @@ File name: port1_read_10_seq_cs1.svh
 */
 
 class port1_read_10_seq_cs1 extends port1_base_seq;
-
+      `uvm_object_utils(port1_read_10_seq_cs1)
+	
+      function new(string name ="port1_read_10_seq_cs1");
+		          super.new(name);
+	    endfunction
+	
+	
+	task body();
+		    req=port1_transaction::type_id::create("req");
+		
+		    for (int i = 0; i < 10; i++) begin
+	
+             //	assert(req.randomize() with {req.cs1 ==0; req.we0==1; });
+              req.randomize() with {req.cs1 ==0; req.we0==1;};
+        			start_item(req);
+        			finish_item(req);
+        			get_response(rsp);
+        			
+		end
+	endtask
 endclass
