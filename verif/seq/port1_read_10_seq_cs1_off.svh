21cff8662d2c334cb4dcb5f058a4df5586cd997d
Pau-Nunez <133161674+Pau-Nunez@users.noreply.github.com>
2023-07-26 10:10:17 -0600

    port1_read_10_seq_cs1_off.svh updated

--- verif/seq/port1_read_10_seq_cs1_off.svh
+++ verif/seq/port1_read_10_seq_cs1_off.svh
@@ -6,21 +6,27 @@ Version: 1
 File name: port1_read_10_seq_cs1_off.svh
 */
 
-class port1_read_10_seq_cs1_off extends port1_base_seq;
-   `uvm_object_utils(port1_read_10_seq_cs1_off)
+class port1_read_10_seq_cs1_OFF extends port1_base_seq;
+   `uvm_object_utils(port1_read_10_seq_cs1_OFF)
+	
+   function new(string name ="port1_read_10_seq_cs1_off");
+		super.new(name);
+	endfunction
+	
+	
+	task body();
+		req=port1_transaction::type_id::create("req");
+		
+		for (int i = 0; i < 10; i++) begin
+	
+         //assert(req.randomize() with {req.cs1 ==1;req.we1==1; });
+         req.randomize() with {req.cs1 ==1;req.we1==1;};
+			start_item(req);
+			finish_item(req);
+			get_response(rsp);
 
-   function new(string name = "");
-      super.new(name);
-   endfunction
-
-   task body();
-      req = port1_transaction::type_id::create("req");
-      start_item(req);
-      req.randomize() with { req.width == env.env_cfg.agent_cfg.cs1;
-                            req.heigth == env.env_cfg.agent_cfg.cs1; };
-                  `uvm_info(get_name(),"Generating sequence item",UVM_HIGH)
-                  `uvm_info(get_name(),$sformatf("%s",req.sprint(uvm_default_line_printer)),UVM_DEBUG)
-      finish_item(req);
-   endtask
-
+		end
+	endtask
+	
+	
 endclass
