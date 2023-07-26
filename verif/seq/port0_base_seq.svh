5bed97db6de583402c79bad19e90af41c33e734a
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-26 09:18:15 -0600

    [seq] port0_base_seq.svh updated

--- verif/seq/port0_base_seq.svh
+++ verif/seq/port0_base_seq.svh
@@ -1,21 +1,15 @@
 
 class port0_base_seq extends uvm_sequence #(port0_transaction);
   `uvm_object_utils(port0_base_seq)
+    
+      port0_seq_cfg_obj port0_cfg;
 
-  port0_seq_cfg_obj seq_cfg;
-
-  function new(string name ="");
-    super.new(name);
-  endfunction
-
-  task pre_body();
-	/*
-    req=port0_transaction::type_id::create("req");
-    start_item(req);
-    assert(req.randomize());//with {req.a>req.b;});
-    finish_item(req);
-    get_response(rsp);
-	*/
-  endtask
-
+	function new(string name ="");
+  		super.new(name);
+ 	endfunction
+    
+   	 task pre_body();
+	 	if(!uvm_config_db#(port0_seq_cfg_obj)::get(null,"ram_base_test.ram_env.agent_port0","port0_cfg",port0_cfg))
+			`uvm_fatal(get_name(), "Failed to get sequence configuration object")
+	endtask
 endclass
