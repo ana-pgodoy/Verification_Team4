f3c2e3a389ec6f53bb0e5045096d06b9ba34b06e
gDiode <a.g.ben.min@gmail.com>
2023-07-26 16:02:33 -0600

    [seq] changed to cadance style pathing

--- verif/seq/port0_base_seq.svh
+++ verif/seq/port0_base_seq.svh
@@ -9,7 +9,8 @@ class port0_base_seq extends uvm_sequence #(port0_transaction);
  	endfunction
     
    	 task pre_body();
-	 	if(!uvm_config_db#(port0_seq_cfg_obj)::get(null,"ram_base_test.enviroment.agent_0","port0_cfg",port0_cfg))
+	 	//if(!uvm_config_db#(port0_seq_cfg_obj)::get(null,"ram_base_test.enviroment.agent_0","port0_cfg",port0_cfg)) EDA CONFIG PATH
+		if(!uvm_config_db#(port0_seq_cfg_obj)::get(null,"uvm_test_top.enviroment.agent_0","port0_cfg",port0_cfg)) //CADANCE PATHING
 			`uvm_fatal(get_name(), "Failed to get sequence configuration object")
 	endtask
 endclass
