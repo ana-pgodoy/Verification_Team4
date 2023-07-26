7c62d9a8304158609f741c3ff1a8f3b41610826a
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-26 09:21:42 -0600

    [seq] ram_seq_pkg.sv updated

--- verif/seq/ram_seq_pkg.sv
+++ verif/seq/ram_seq_pkg.sv
@@ -9,12 +9,13 @@ package ram_seq_pkg;
 	  `include "port1_base_seq.svh"
 	  `include "port0_read_10_seq_cs0.svh"
 	  `include "port0_read_10_seq_cs0_OFF.svh"
+	  `include "port0_read_10_seq_cs0_fast.svh"
 	  `include "port0_read_10_seq_cs0_slow.svh"
+	  `include "port0_rewrite_10_seq_we.svh"
 	  `include "port0_write_10_seq_we.svh"
 	  `include "port1_read_10_seq_cs1_off.svh"
 	  `include "port1_read_10_seq_cs1.svh"
+	 `include "port1_fast_read_10_seq.svh"
 	  `include "port1_slow_read_10_seq.svh"
   
-  
-
 endpackage
