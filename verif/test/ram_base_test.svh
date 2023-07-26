0d9b51c2b0d6ac80b12616d5661f84ba6d1a5256
ana-pgodoy <131797963+ana-pgodoy@users.noreply.github.com>
2023-07-26 09:16:37 -0600

    [testram_base_test.svh

--- verif/test/ram_base_test.svh
+++ verif/test/ram_base_test.svh
@@ -6,7 +6,7 @@ Version: 01
 File name: ram_base_test.svh
 */
 
-class ram_base_test extends uvm_test;
+  class ram_base_test extends uvm_test;
 	`uvm_component_utils(ram_base_test )
 
 	ram_env enviroment;
@@ -37,17 +37,21 @@ class ram_base_test extends uvm_test;
 			
 	repeat (10) begin 
 		port0_cfg =port0_seq_cfg_obj::type_id::create("port0_cfg");
-		port0_cfg.randomize();
+		//port0_cfg.randomize();
 		port1_cfg =port1_seq_cfg_obj::type_id::create("port1_cfg");
-		port1_cfg.randomize();
+		//port1_cfg.randomize();
 
 		uvm_config_db#(port0_seq_cfg_obj)::set(this,"ram_env.agent_port0","port0_cfg",port0_cfg);
 		uvm_config_db#(port1_seq_cfg_obj)::set(this,"ram_env.agent_port1","port1_cfg",port1_cfg);
 
-		port0_base =port0_base_seq::type_id::create("port0_base");
-		port0_base.start(ram_env.agent_port0.port0_sqr);
+		port0_base=port0_base_seq::type_id::create("port0_base");
+      
+      port0_base.start(enviroment.agent_0.sequencer);
+      
 		port1_base =port1_base_seq::type_id::create("port1_base");
-		port1_base.start(ram_env.agent_port1.port1_sqr);
+      
+      port1_base.start(enviroment.agent_1.sequencer);
+      
 	end
 		phase.drop_objection(this);
 	endtask
