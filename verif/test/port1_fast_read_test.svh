b7292529e6bd1f941aefcf97f25218d413e36e86
Xrist12 <133039993+Xrist12@users.noreply.github.com>
2023-07-27 12:45:07 -0600

    Update port1_fast_read_test.svh

--- verif/test/port1_fast_read_test.svh
+++ verif/test/port1_fast_read_test.svh
@@ -9,8 +9,8 @@ File name: port1_fast_read_test.svh
 class port1_fast_read_test extends ram_base_test; // specific test
 	`uvm_component_utils(port1_fast_read_test)
 
-	port1_fast_read_10_seq  fast_read_seq;
-   port1_seq_cfg_obj       seq_cfg;
+	port1_fast_read_10_seq  fast_read_seq_port1;
+   	port1_seq_cfg_obj       seq_cfg;
 
 	function new(string name, uvm_component parent);
 		super.new(name,parent);
@@ -19,16 +19,16 @@ class port1_fast_read_test extends ram_base_test; // specific test
 	task run_phase(uvm_phase phase);
 		phase.raise_objection(this);
 
-      seq_cfg = port1_seq_cfg_obj::type_id::create("seq_cfg");
+      		seq_cfg = port1_seq_cfg_obj::type_id::create("seq_cfg");
 
 		#1us;
 
 		repeat(10) begin
-         seq_cfg.randomize();
+        		seq_cfg.randomize();
 			//seq_cfg.addr1 = ;
-         uvm_config_db#(port1_seq_cfg_obj)::set(this,"ram_env.agent_1","seq_cfg",seq_cfg);
-			fast_read_seq = port1_fast_read_10_seq::type_id::create("fast_read_seq");
-			fast_read_seq.start(enviroment.agent_1.sequencer);
+         		uvm_config_db#(port1_seq_cfg_obj)::set(this,"ram_env.agent_1","seq_cfg",seq_cfg);
+			fast_read_seq_port1 = port1_fast_read_10_seq::type_id::create("fast_read_seq_port1");
+			fast_read_seq_port1.start(enviroment.agent_1.sequencer);
 		end
 
 		#1us;
